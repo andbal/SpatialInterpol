9d47d7dae4129cb158bf39fc14bac81366f796ea
Andrea Balotti <andrea.balotti@hotmail.it>
2016-06-16 17:35:02 +0200

    *minor* - armonised default option

--- R/OrdKrig.R
+++ R/OrdKrig.R
@@ -8,7 +8,7 @@
 # library(SpatialPosition)
 
 OrdKrig <- function ( wpath = "/home/jbre/R/OrdKrig", 
-                      datafolder = "raw", rastermask = "mask/Mask_master.tif",
+                      datafolder = "master", rastermask = "mask/Mask_master.tif",
                       inverseDistWeigths = FALSE, local=TRUE,
                       variable = "Humus____",  npix = 100,
                       cutoff = c("AdigeVenosta"=400, "Adige"=400, "Venosta"=450), 
